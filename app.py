182c824c3be47a2f5f9cd6fb99c0e13df33e5efa
Mark Perez <81707384+MadbnM@users.noreply.github.com>
2023-05-02 01:19:19 +0800

    add login and register page

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)

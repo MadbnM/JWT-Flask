13612e94672a2cb7c0e63e303117c92565c24fb9
Mark Perez <81707384+MadbnM@users.noreply.github.com>
2023-05-04 13:18:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,19 @@ class User(db.Model):
     email = db.Column(db.String(70), unique = True)
     password = db.Column(db.String(80))
 
+ # Define the Role data-model
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
 with app.app_context():
     db.create_all()
     
